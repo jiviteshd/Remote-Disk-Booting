ddfe97ebbd5000d2a82506d72b8bf7bf0db97f6c
Jivitesh Dalavi <jiviteshdalavi@gmail.com>
2021-03-14 17:48:32 +0530

    Update install.sh
    Install os on defined directory

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ yum -y install syslinux xinetd tftp-server
 
 yum -y install dracut-network nfs-utils
 mkdir -p /var/lib/tftpboot/centos7/root
-
+yum groups -y install "Server with GUI" --releasever=7 --installroot=/var/lib/tftpboot/centos7/root/
 
 bash change.sh
